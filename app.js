c4fbc408be69d445eead7755f0bd5d107fc94188
ln-dev7 <leonelngoya@gmail.com>
2022-10-11 03:44:52 +0100

    update

--- app.js
+++ app.js
@@ -1,19 +1,62 @@
-const initButtonClose = document.querySelector('.init-button__close')
-const initButtonOpen = document.querySelector('.init-button__open')
-const btnArrow = document.querySelector('.chat-admin-options .btn-arrow')
-const chat = document.querySelector('.chat')
-const initText = document.querySelector('.init-text')
+const initButtonClose = document.querySelector(".init-button__close");
+const initButtonOpen = document.querySelector(".init-button__open");
+const btnArrow = document.querySelector(".chat-admin-options .btn-arrow");
+const chat = document.querySelector(".chat");
+const initText = document.querySelector(".init-text");
+const chatBody = document.querySelector(".chat-body");
+const chatInput = document.querySelector(".chat-input input");
 
-initButtonClose.addEventListener('click', () => {
-    initButtonClose.style.transform= 'translateX(-50%)';
-    initButtonOpen.style.transform= 'translateX(-50%)';
-    initText.style.display = "none";
-    chat.classList.add('show');
-})
+const chatBodyScroll = () => {
+  chatBody.scrollTop = chatBody.scrollHeight;
+};
 
-btnArrow.addEventListener('click', () => {
-    initButtonClose.style.transform= 'translateX(50%)';
-    initButtonOpen.style.transform= 'translateX(50%)';
-    initText.style.display = "flex";
-    chat.classList.remove('show');
-})
+initButtonClose.addEventListener("click", () => {
+  chatBodyScroll();
+  initButtonClose.style.transform = "translateX(-50%)";
+  initButtonOpen.style.transform = "translateX(-50%)";
+  initText.style.display = "none";
+  chat.classList.add("show");
+});
+
+btnArrow.addEventListener("click", () => {
+  initButtonClose.style.transform = "translateX(50%)";
+  initButtonOpen.style.transform = "translateX(50%)";
+  initText.style.display = "flex";
+  chat.classList.remove("show");
+});
+
+initButtonOpen.addEventListener("click", () => {
+  if (chatInput.value == "") {
+    chatInput.classList.add("wrong");
+    setTimeout(() => {
+      chatInput.classList.remove("wrong");
+    }, 1000);
+  } else {
+    const newSend = document.createElement("div");
+    newSend.classList.add("send");
+    newSend.innerText = chatInput.value;
+    chatBody.appendChild(newSend);
+    chatBodyScroll();
+
+    const writeReceive = document.createElement("div");
+    writeReceive.classList.add("receive");
+    writeReceive.classList.add("write");
+    writeReceive.innerText = "Ecrit ...";
+    chatBody.appendChild(writeReceive);
+    chatBodyScroll();
+
+    const newReceive = document.createElement("div");
+    newReceive.classList.add("receive");
+    newReceive.innerHTML =
+      "Je comprends pas ce que tu dis ... Tu peux visiter mon portfolio <a href='https://lndev.me'>https://lndev.me</a>";
+
+    setTimeout(() => {
+      chatBody.appendChild(newReceive);
+      chatBodyScroll();
+      chatBody.removeChild(writeReceive);
+      chatBodyScroll();
+    }, 1500);
+
+    chatInput.value = "";
+  }
+});
