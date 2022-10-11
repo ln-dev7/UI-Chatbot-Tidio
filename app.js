bcabb64cee680d08bed9ea3340a270215241355d
ln-dev7 <leonelngoya@gmail.com>
2022-10-11 03:02:36 +0100

    update

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const initButtonClose = document.querySelector('.init-button__close')
 const initButtonOpen = document.querySelector('.init-button__open')
 const btnArrow = document.querySelector('.chat-admin-options .btn-arrow')
 const chat = document.querySelector('.chat')
+const initText = document.querySelector('.init-text')
 
 initButtonClose.addEventListener('click', () => {
     initButtonClose.style.transform= 'translateX(-50%)';
     initButtonOpen.style.transform= 'translateX(-50%)';
+    initText.style.display = "none";
     chat.classList.add('show');
 })
 
 btnArrow.addEventListener('click', () => {
     initButtonClose.style.transform= 'translateX(50%)';
     initButtonOpen.style.transform= 'translateX(50%)';
+    initText.style.display = "flex";
     chat.classList.remove('show');
 })
